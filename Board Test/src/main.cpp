b7b34741ff55b2a710864a2f6ffa84ac8754d8e0
spacejunkl <cbscott6163@gmail.com>
2023-09-22 22:01:03 -0400

    commiting test branch

--- Board Test/src/main.cpp
+++ Board Test/src/main.cpp
@@ -5,5 +5,21 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("This is a working test! :)"); 
+  Serial.println("This is a working test! :)!!"); 
+  Serial.println("this is a change");
 }
+
+
+//Git commands to go over 
+  //pull, fetch 
+  //creating branches, switching branches, doing both at the same time, checking out, deleting
+  //how to see which branch you are on (in both terminal and VS), switching
+  //add file/staging, committing, pushing 
+  //merging branches 
+  //Git policies
+    //dev branch 
+    //don't push to main or dev branch 
+    //don't create 2 million branches on branches on branches 
+
+  //pull requests 
+   

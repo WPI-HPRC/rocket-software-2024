1898ee0e994cdfab5a7d112da40b8a7b75f1e708
Daniel Pearson <dpear0915@gmail.com>
2023-09-30 16:50:34 -0400

    Added basic state machine into Rocket Flight Computer

--- RocketFlightComputer/src/main.cpp
+++ RocketFlightComputer/src/main.cpp
@@ -1,7 +1,59 @@
 #include <Arduino.h>
+#include <lib/MetroTimer/Metro.h>
+#include <Wire.h>
+#include <SPI.h>
+
+
+#define LOOP_RATE 100
+
+Metro timer = Metro(1000 / LOOP_RATE);
+int counter = 0;
+uint32_t timestamp;
+
+long loopStartTime;
+long loopTime;
+long previousTime;
+
+enum RocketState { 
+  STARTUP,
+  LOOP
+};
+
+RocketState state = STARTUP;
 
 void setup() {
-}
+  Serial.begin(115200); 
+
+  Wire.begin();
+  Wire.setClock(400000);
+
+  timer.reset();
+  previousTime = millis();
+  loopStartTime = millis();
+
+  state = LOOP;
+};
+
+void readSensors() {
+
+};
 
 void loop() {
-}
+  
+  if(timer.check() == 1) {
+    switch (state)
+    {
+      case STARTUP:
+        Serial.println("+=HPRC FLIGHT COMPUTER=+");
+        Serial.println("Starting up...");
+        /* code */
+        break;
+      case LOOP:
+        readSensors();
+
+        break;
+    };
+
+    counter++;
+  }
+};

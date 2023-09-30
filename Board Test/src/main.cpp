3e6a651ccc08a6ff263a1f07e35fc45dc981f391
Rit Garg <100240647+fielia@users.noreply.github.com>
2023-09-30 17:47:59 -0400

    working GNSS sensor readings

--- Board Test/src/main.cpp
+++ Board Test/src/main.cpp
@@ -1,26 +1,46 @@
 #include <Arduino.h>
+#include <Wire.h>
+
+#include <SparkFun_u-blox_GNSS_v3.h>
+
+SFE_UBLOX_GNSS myGNSS;
 
 void setup() {
-  Serial.begin(9600); 
+	Serial.begin(9600);
+
+	while(!Serial);
+	Serial.println("GNSS Example:");
+
+	Wire.begin();
+
+	if (myGNSS.begin() == false) {
+		Serial.println(F("u-blox GNSS module not detected at default I2C address. Please check wiring. Freezing."));
+		while (1);
+	}
+
+	// myGNSS.setNMEAOutputPort(Serial);
+}
+
+double prevLat;
+double prevLon;
+
+String getChanged(double lat, double lon) {
+	String state;
+	if (lat == prevLat && lon == prevLon) {
+		state = "false.";
+	} else {
+		state = "true.";
+	}
+	prevLat = lat;
+	prevLon = lon;
+	return state;
 }
 
 void loop() {
-  Serial.println("This is a working test! :)!!"); 
-  Serial.println("this is a change");
-  Serial.println("rnifjwne rogibwehbgoewubwoiuthbgwehbgyub4owubgkw4ebhgu " ); 
+	double lat = myGNSS.getLatitude();
+	double lon = myGNSS.getLongitude();
+	Serial.println("Latitude: " + String(lat) + ". Longitude: " + String(lon) + ". Changed: " + getChanged(lat, lon));
+	
+
+	delay(250); // Don't pound too hard on the I2C bus
 }
-
-
-//Git commands to go over 
-  //pull, fetch 
-  //creating branches, switching branches, doing both at the same time, checking out, deleting
-  //how to see which branch you are on (in both terminal and VS), switching
-  //add file/staging, committing, pushing 
-  //merging branches 
-  //Git policies
-    //dev branch 
-    //don't push to main or dev branch 
-    //don't create 2 million branches on branches on branches 
-
-  //pull requests 
-   

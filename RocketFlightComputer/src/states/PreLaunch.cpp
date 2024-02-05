74b6de508de5b5001617d6de372e12d7c51a9686
Dashiell Elliott <dashiell.elliott@yahoo.com>
2024-02-05 17:50:12 -0500

    PreLauch state logic moved from 2023

--- RocketFlightComputer/src/states/PreLaunch.cpp
+++ RocketFlightComputer/src/states/PreLaunch.cpp
@@ -6,15 +6,31 @@
 
 PreLaunch::PreLaunch(struct Sensors *sensors) : State(sensors) {}
 
+float PreLaunch::avgAccelZ() {
+	float sum = 0;
+	uint8_t len = sizeof(this->accelReadingBuffer) / sizeof(float);
+
+	for (uint8_t i = 0; i < len; i++) {
+		sum += this->accelReadingBuffer[i];
+	}
+
+	return sum / len;
+}
+
 void PreLaunch::initialize_impl() {}
 
 void PreLaunch::loop_impl() {
-	// Read bno for example
 	if(!sensorPacket.gpsLock) {
 		// Serial.println("Acquiring Gps Lock...");
 		// delay(100);
 		return;
 	};
+
+	if (this->stateEstimatorInitialized) {
+		this->accelReadingBuffer[this->buffIdx++] = this->sensorPacket.accelZ;
+		this->buffIdx %= sizeof(this->accelReadingBuffer) / sizeof(float);
+		return;
+	}
 
 	// float r_adj = Utility::r_earth + sensorPacket.gpsAltMSL; // [m]
 	float N_earth = Utility::a_earth / sqrt(1 - pow(Utility::e_earth,2) * pow(sin(sensorPacket.gpsLat), 2));
@@ -24,7 +40,6 @@ void PreLaunch::loop_impl() {
 	float Z_0 = (((Utility::b_earth*Utility::b_earth)/(Utility::a_earth*Utility::a_earth))*N_earth + sensorPacket.gpsAltAGL) * sin(sensorPacket.gpsLat * DEG_TO_RAD);
 	// float Z_0 = (N_earth*(1-pow(Utility::e_earth,2))+sensorPacket.gpsAltAGL)*sin(sensorPacket.gpsLat);
 
-
 	Serial.println("[Pre-Launch] Initial GPS Position Acquired!");
 	Serial.print("Latitude: "); Serial.println(sensorPacket.gpsLat, 4);
 	Serial.print("Longitude: "); Serial.println(sensorPacket.gpsLong, 4);
@@ -33,8 +48,9 @@ void PreLaunch::loop_impl() {
 	// Intialize EKF
 	BLA::Matrix<10> x_0 = {1,0,0,0, X_0, Y_0, Z_0, 0,0,0};
 	// BLA::Matrix<4> x_0 = {1,0,0,0};
-	ekf = new StateEstimator(x_0, 0.025);
-	// ekf = new StateEstimator(x_0, 0.025);
+	this->ekf = new StateEstimator(x_0, 0.025);
+
+	this->stateEstimatorInitialized = true;
 }
 
 //! @details If we are separating this from `Launch`, we need a time limit on this state or something
@@ -46,7 +62,7 @@ State *PreLaunch::nextState_impl()
 		return new Debug(this->sensors, this->ekf);
 	}
 
-	if (this->currentTime > MAX_PRELAUNCH_TIME && sensorPacket.gpsLock) {
+	if (sensorPacket.gpsLock && this->avgAccelZ() > LAUNCH_ACCEL_THRESHOLD) {
 		return new Launch(sensors);
 	}
 	return nullptr;
--- RocketFlightComputer/src/states/PreLaunch.h
+++ RocketFlightComputer/src/states/PreLaunch.h
@@ -2,13 +2,14 @@
 #include "State.h"
 #include "Sensors.h"
 
-//! @brief max number of milliseconds we can remain in the prelaunch (initialization) state
-#define MAX_PRELAUNCH_TIME 3000
 class PreLaunch : public State {
 	_STATE_CLASS_IMPLS_
 	public:
 		PreLaunch(struct Sensors *sensors);
 	private:
 		StateEstimator * ekf;
-	
+		bool stateEstimatorInitialized = false;
+		float accelReadingBuffer[10];
+		uint8_t buffIdx = 0;
+		float avgAccelZ();
 };

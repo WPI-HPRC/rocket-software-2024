2dd64988eb737b199324b0888de885ee3bc783d5
Fluxx7 <nwa31104@gmail.com>
2024-02-04 20:39:37 -0500

    Copied over legacy code without changing anything

--- RocketFlightComputer/src/states/Launch.cpp
+++ RocketFlightComputer/src/states/Launch.cpp
@@ -6,7 +6,24 @@ Launch::Launch(struct Sensors *sensors) : State(sensors) {}
 
 void Launch::initialize_impl() {}
 
-void Launch::loop_impl() {}
+void Launch::loop_impl() {
+	// Stay in this state for at least 3 seconds to prevent airbrake activation
+	logData();
+	if (boostTimer.check() == 1)
+	{
+		boostTimerElapsed = true;
+	}
+
+	if (boostTimerElapsed)
+	{
+		if (motorBurnoutDetect())
+		{
+			state_start = millis();
+			avionicsState = COAST;
+			break;
+		}
+	}
+}
 
 State *Launch::nextState_impl() {
 	return nullptr;

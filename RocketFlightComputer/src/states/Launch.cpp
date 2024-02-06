6f66d18c7842bbba07a5b84714990f4784ec01d4
Fluxx7 <nwa31104@gmail.com>
2024-02-05 19:04:03 -0500

    started actually making code

--- RocketFlightComputer/src/states/Launch.cpp
+++ RocketFlightComputer/src/states/Launch.cpp
@@ -1,5 +1,6 @@
 #include "State.h"
 #include "Launch.h"
+#include "Coast.h"
 #include "Sensors.h"
 
 Launch::Launch(struct Sensors *sensors) : State(sensors) {}
@@ -7,27 +8,36 @@ Launch::Launch(struct Sensors *sensors) : State(sensors) {}
 void Launch::initialize_impl() {}
 
 void Launch::loop_impl() {
-	// Stay in this state for at least 3 seconds to prevent airbrake activation
-	logData();
-	if (boostTimer.check() == 1)
-	{
-		boostTimerElapsed = true;
-	}
+	// old motorBurnoutDetect code
+	// accel value gets updated in sensor reading fcn
+    // add to cyclic buffer
+    transitionBufAcc[transitionBufIndAcc] = sensorPacket.ac_z;
+    // take running average value
+    float sum = 0.0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += transitionBufAcc[i];
+    }
+    sum = sum / 10.0;
 
-	if (boostTimerElapsed)
-	{
-		if (motorBurnoutDetect())
-		{
-			state_start = millis();
-		}
-	}
+    transitionBufIndAcc = (transitionBufIndAcc + 1) % 10;
+    // compare running average value to defined threshold
+    if (sum < 0)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            transitionBufAcc[j] = 0;
+        }
+        transitionBufIndAcc = 0;
+        Serial.println("Motor burnout detected!");
+    }
 }
 
 State *Launch::nextState_impl() {
-	// TODO: Figure out and implement the conditions for switch to Coast state
-	// Switch to Coast when the conditions are right
-	if (0) {
-		return new Coast(sensors)
+	// Stay in this state for at least 3 seconds to prevent airbrake activation
+	//Switch to Coast when the conditions are right (TODO: fix this shitty comment)
+	if (this->currentTime > MAX_LAUNCH_TIME && motorBurnout) {
+		return new Coast(sensors);
 	}
 	return nullptr;
 }
--- RocketFlightComputer/src/states/Launch.h
+++ RocketFlightComputer/src/states/Launch.h
@@ -2,8 +2,12 @@
 #include "State.h"
 #include "Sensors.h"
 
+#define MAX_LAUNCH_TIME 3000
+
 class Launch : public State {
 	_STATE_CLASS_IMPLS_
 	public:
 		Launch(struct Sensors *sensors);
+	private:
+		bool motorBurnout = false;
 };

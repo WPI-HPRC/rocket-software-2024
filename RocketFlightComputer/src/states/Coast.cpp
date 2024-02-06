7c1fe0cb38ceeee480eaaaba6c29a7780ebdf9b9
Rit Garg <100240647+fielia@users.noreply.github.com>
2024-02-05 20:05:01 -0500

    bones of the implementation

--- RocketFlightComputer/src/states/Coast.cpp
+++ RocketFlightComputer/src/states/Coast.cpp
@@ -1,13 +1,43 @@
 #include "Coast.h"
-#include "State.h"
 
+#include "DrogueDescent.h"
+// #include "Abort.h" <- needed as failsafe
+#include "State.h"
 
 Coast::Coast() {}
 
 void Coast::initialize_impl() {}
 
-void Coast::loop_impl() {}
+void Coast::loop_impl() {
+    // Velocity value gets updated in sensor reading fcn
+    // add to cyclic buffer
+    transitionBufVelVert[transitionBufIndVelVert] = stateStruct.vel_vert;
+    // take running average value
+    float sum = 0.0;
+    for (int i = 0; i < 10; i++) {
+        sum += transitionBufVelVert[i];
+    }
+    sum = sum / 10.0;
 
+    transitionBufIndVelVert = (transitionBufIndVelVert + 1) % 10;
+    // if average vertical velocity is negative, passed apogee
+    if (sum < 0) {
+        for (int j = 0; j < 10; j++) {
+            transitionBufVelVert[j] = 0;
+        }
+        transitionBufIndVelVert = 0;
+        // Serial.println("Apogee detected!");
+        apogeePassed = true
+    }
+}
+
+//! @details max 8 seconds until deploy
 State *Coast::nextState_impl() {
-	return nullptr;
+    if (this->acceleration > 10) {
+        // return new Abort();
+    }
+    if (this->currentTime > MAX_COAST_TIME || apogeePassed) {
+        return new DrogueDescent();
+    }
+    return nullptr;
 }
--- RocketFlightComputer/src/states/Coast.h
+++ RocketFlightComputer/src/states/Coast.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "State.h"
 
+#define MAX_COAST_TIME 30000
 class Coast : public State {
 	_STATE_CLASS_IMPLS_
 	public:
-		Coast();
+	Coast();
+	private:
+	boolean apogeePassed = false;
 };

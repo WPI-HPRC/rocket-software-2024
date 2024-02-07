d087b3507434ae0d2e5ad762d36f4b5f3e31cad0
Kai Davidson <>
2024-02-06 21:43:30 -0500

    Look I did something in MainDescent, this needs testing and also we should probably remove this fake buffer I made in it.

--- RocketFlightComputer/src/states/MainDescent.cpp
+++ RocketFlightComputer/src/states/MainDescent.cpp
@@ -6,8 +6,54 @@ MainDescent::MainDescent(struct Sensors *sensors, StateEstimator *stateEstimator
 
 void MainDescent::initialize_impl() {}
 
-void MainDescent::loop_impl() {}
+void MainDescent::loop_impl() {    
+    // Data is already logged within the state class    
+
+    // Altitude below 20 meters
+    // Altitude value gets updated in sensor reading function
+
+    //Create buffer of last 10 altitude values
+    LPS25_data curr_pressure = sensors->barometer->read();
+    curr_altitude = pressureToAltitude(curr_pressure.pressure);
+    // Move all previous buffer values up one and add new value to the front
+    for(int i = 8; i>0; i--)
+    {
+        altitudeBuffer[i+1] = altitufeBuffer[i];
+    }
+    altitudeBuffer[0] = curr_altitude;
+    
+    
+    if(altitudeBuffer[9] != 0){
+        // Take running average value
+        float avg_sum = 0.0;
+        for (int i = 0; i < 10; i++)
+        {
+            avg_sum += altitudeBuffer[i];
+        }
+        float avg = avg_sum / 10.0;
+
+        // Calculate mean difference from average
+        float avg_std_sum = 0.0;
+        for (int i = 0; i < 10; i++)
+        {
+            avg_std_sum += abs(altitudeBuffer[i] - avg);
+        }
+        float avg_std = avg_std_sum / 10.0;
+
+        // If altitude is nearly constant and below landing threshold
+        if (avg_std < 5 && curr_altitude <= LAND_THRESHOLD)
+        {            
+            nextState();
+        }
+    }
+    // timeout after 100 seconds if other checks fail
+    if (timeout(MAIN_DESCENT_TIMEOUT))
+    {
+        nextState();
+    }
+}
 
 State *MainDescent::nextState_impl() {
+    float state_start = millis();
 	return nullptr;
 }
--- RocketFlightComputer/src/states/MainDescent.h
+++ RocketFlightComputer/src/states/MainDescent.h
@@ -2,8 +2,13 @@
 #include "State.h"
 #include "Sensors.h"
 
+#define LAND_THRESHOLD 20 // Apparently landing height in meters
+#define MAIN_DESCENT_TIMEOUT 100 * 1000 // 100 seconds
+
 class MainDescent : public State {
 	_STATE_CLASS_IMPLS_
+    private:
+        float altitudeBuffer[10];
 	public:
 		MainDescent(struct Sensors *sensors);
 };

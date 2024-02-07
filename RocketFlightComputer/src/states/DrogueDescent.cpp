9986ab80d0fe4216d5275fca9662ea370db3c022
Brendon Peters <Brendon Peters>
2024-02-06 19:54:28 -0500

    Drogue Descent updated:2/6/24. Reformed old code to use new logic

--- RocketFlightComputer/src/states/DrogueDescent.cpp
+++ RocketFlightComputer/src/states/DrogueDescent.cpp
@@ -1,13 +1,55 @@
 #include "State.h"
 #include "DrogueDescent.h"
 
+int sumDrogueDescentVel = 0;
+const float METER_CONVERSION = 0.3048;
 
-DrogueDescent::DrogueDescent() {}
+float DROGUE_DESCENT_THRESHOLD; // tbd
 
-void DrogueDescent::initialize_impl() {}
 
-void DrogueDescent::loop_impl() {}
+DrogueDescent::DrogueDescent() 
+{
 
-State *DrogueDescent::nextState_impl() {
+}
+
+void DrogueDescent::initialize_impl() 
+{
+	this->currentTime = millis();
+}
+
+void DrogueDescent::loop_impl() 
+{
+	while(altitude < (1500 * METER_CONVERSION)){
+		logData();
+
+    	// ABORT Case
+    	// 10 second timeout
+    	if (/*New abort condition*/)
+    	{
+        	nextState_impl('a');
+			break;
+    	}
+           
+    	// detect altitude drop below 1500ft
+    	if (altitude < (1500 * METER_CONVERSION))
+    	{
+        	nextState_impl('m');
+        	break;
+    	}
+	}
+            
+}
+
+State *DrogueDescent::nextState_impl(char nextStateChar) 
+{
+	if(nextStateChar == 'a')
+	{
+		return *Abort; //do we have an abort state for the new state machine?
+	}
+	else if(nextStateChar == 'm')
+	{
+		return *MainDescent; 
+	}
+
 	return nullptr;
 }

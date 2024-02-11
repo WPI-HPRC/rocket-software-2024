d4ba1ee24f9aaa66de7bdbd141dee4bb8d599151
Cutter Beck <cbeckdevelopment@gmail.com>
2024-02-11 15:17:50 -0500

    Restructured code

--- RocketFlightComputer/src/states/MainDescent.cpp
+++ RocketFlightComputer/src/states/MainDescent.cpp
@@ -6,54 +6,37 @@ MainDescent::MainDescent(struct Sensors *sensors, StateEstimator *stateEstimator
 
 void MainDescent::initialize_impl() {}
 
-void MainDescent::loop_impl() {    
-    // Data is already logged within the state class    
-
-    // Altitude below 20 meters
-    // Altitude value gets updated in sensor reading function
-
-    //Create buffer of last 10 altitude values
-    LPS25_data curr_pressure = sensors->barometer->read();
-    curr_altitude = pressureToAltitude(curr_pressure.pressure);
-    // Move all previous buffer values up one and add new value to the front
-    for(int i = 8; i>0; i--)
-    {
-        altitudeBuffer[i+1] = altitufeBuffer[i];
-    }
-    altitudeBuffer[0] = curr_altitude;
+void MainDescent::loop_impl() {
+    float curr_altitude = sensorPacket.altitude;
+    altitudeBuffer[altitudeBufferIndex] = curr_altitude;
     
-    
-    if(altitudeBuffer[9] != 0){
-        // Take running average value
+    if (altitudeBufferIndex == 9) {
+        // take the average of the full buffer
         float avg_sum = 0.0;
+        float average = 0.0;
         for (int i = 0; i < 10; i++)
         {
             avg_sum += altitudeBuffer[i];
         }
-        float avg = avg_sum / 10.0;
+        average = avg_sum / 10.0;
 
-        // Calculate mean difference from average
+        // find average standard deviation
         float avg_std_sum = 0.0;
+        float avg_std = 0.0;
         for (int i = 0; i < 10; i++)
         {
-            avg_std_sum += abs(altitudeBuffer[i] - avg);
+            avg_std_sum += abs(altitudeBuffer[i] - average);
         }
-        float avg_std = avg_std_sum / 10.0;
+        avg_std = avg_std_sum / 10.0;
+    }
 
-        // If altitude is nearly constant and below landing threshold
-        if (avg_std < 5 && curr_altitude <= LAND_THRESHOLD)
-        {            
-            nextState();
-        }
-    }
-    // timeout after 100 seconds if other checks fail
-    if (timeout(MAIN_DESCENT_TIMEOUT))
-    {
-        nextState();
-    }
+    altitudeBufferIndex = (altitudeBufferIndex + 1) % 10;
 }
 
 State *MainDescent::nextState_impl() {
-    float state_start = millis();
+    if (this->currentTime > MAIN_DESCENT_TIMEOUT && avg_std < 5 && curr_altitude <= LAND_THRESHOLD) {
+        return new Recovery(sensors, stateEstimator, flash);
+    }
+
 	return nullptr;
 }
--- RocketFlightComputer/src/states/MainDescent.h
+++ RocketFlightComputer/src/states/MainDescent.h
@@ -2,13 +2,16 @@
 #include "State.h"
 #include "Sensors.h"
 
-#define LAND_THRESHOLD 20 // Apparently landing height in meters
+#define LAND_THRESHOLD 20               // max height in meters to land
 #define MAIN_DESCENT_TIMEOUT 100 * 1000 // 100 seconds
 
-class MainDescent : public State {
-	_STATE_CLASS_IMPLS_
-    private:
-        float altitudeBuffer[10];
-	public:
-		MainDescent(struct Sensors *sensors, StateEstimator *stateEstimator, FlashChip *flashChip);
+class MainDescent : public State
+{
+    _STATE_CLASS_IMPLS_
+public:
+    MainDescent(struct Sensors *sensors, StateEstimator *stateEstimator, FlashChip *flashChip);
+
+private:
+    float altitudeBuffer[10];
+    int altitudeBufferIndex = 0;
 };

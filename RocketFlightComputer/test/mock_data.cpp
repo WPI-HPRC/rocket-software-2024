6e04969795ae00a860eab435680f9019171c9317
Dashiell Elliott <dashiell.elliott@yahoo.com>
2024-03-23 13:21:37 -0400

    skeleton testing code

---
+++ RocketFlightComputer/test/mock_data.cpp
@@ -0,0 +1,17 @@
+#include <ArduinoFake.h>
+#include "unity.h"
+
+using namespace fakeit;
+
+void setUp() {
+  ArduinoFakeReset();
+}
+
+void test_basic() {
+}
+
+int main(int argc, char** argv) {
+  UNITY_BEGIN();
+
+  return UNITY_END();
+}

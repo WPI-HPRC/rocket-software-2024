2e330a1e33b8834c91ee6335b8ef342c9d63826b
Dashiell Elliott <dashiell.elliott@yahoo.com>
2024-04-20 14:15:26 -0400

    strip out ekf

--- RocketFlightComputer/src/states/00-PreLaunch.cpp
+++ RocketFlightComputer/src/states/00-PreLaunch.cpp
@@ -56,13 +56,13 @@ void PreLaunch::loop_impl()
     Serial.print(", ");
     Serial.println(telemPacket.accelZ);
 
-    if (this->stateEstimator->initialized)
-    {
+    // if (this->stateEstimator->initialized)
+    // {
         this->accelReadingBuffer[this->buffIdx++] = this->telemPacket.accelZ;
         this->buffIdx %= sizeof(this->accelReadingBuffer) / sizeof(float);
         launched = launchDebouncer.checkOut(this->avgAccelZ() > LAUNCH_ACCEL_THRESHOLD);
         return;
-    }
+    // }
 
     // float r_adj = Utility::r_earth + sensorPacket.gpsAltMSL; // [m]
     // float N_earth = Utility::a_earth / sqrt(1 - pow(Utility::e_earth, 2) * pow(sin(sensorPacket.gpsLat), 2));
@@ -86,48 +86,48 @@ void PreLaunch::loop_impl()
     // Serial.println(">");
 
     // Intialize EKF
-    if (!this->stateEstimator->initialized)
-    {
-        // Calculate Initial Quaternion using Accel and Mag
-        // Normalize Acceleration Vector
-        BLA::Matrix<3> a = {telemPacket.accelX, telemPacket.accelY, telemPacket.accelZ};
-        float aLen = BLA::Norm(a);
-        if (aLen != 0) {
-            a /= aLen;
-        }
+    // if (!this->stateEstimator->initialized)
+    // {
+    //     // Calculate Initial Quaternion using Accel and Mag
+    //     // Normalize Acceleration Vector
+    //     BLA::Matrix<3> a = {telemPacket.accelX, telemPacket.accelY, telemPacket.accelZ};
+    //     float aLen = BLA::Norm(a);
+    //     if (aLen != 0) {
+    //         a /= aLen;
+    //     }
 
-        // Normalize Magnetometer Vector
-        BLA::Matrix<3> m = {telemPacket.magX, telemPacket.magY, telemPacket.magZ};
-        float mLen = BLA::Norm(m);
-        if (mLen != 0) {
-            m /= mLen;
-        }
+    //     // Normalize Magnetometer Vector
+    //     BLA::Matrix<3> m = {telemPacket.magX, telemPacket.magY, telemPacket.magZ};
+    //     float mLen = BLA::Norm(m);
+    //     if (mLen != 0) {
+    //         m /= mLen;
+    //     }
 
-        // Observation Matrix
+    //     // Observation Matrix
 
-        BLA::Matrix<3> crossProd1 = Utility::crossProduct(a, m);
-        BLA::Matrix<3> crossProd2 = Utility::crossProduct(a,crossProd1); 
+    //     BLA::Matrix<3> crossProd1 = Utility::crossProduct(a, m);
+    //     BLA::Matrix<3> crossProd2 = Utility::crossProduct(a,crossProd1); 
 
-        BLA::Matrix<4> q_0 = {
-            0.5 * sqrt(1 + a(0) + crossProd1(1) + crossProd2(2)),
-            0.5 * std::copysign(sqrt(1 + a(0) - crossProd1(1) - crossProd2(2)), crossProd2(1) - crossProd1(2)),
-            0.5 * std::copysign(sqrt(1 - a(0) + crossProd1(1) - crossProd2(2)), crossProd1(2) - crossProd2(1)),
-            0.5 * std::copysign(sqrt(1 - a(0) - crossProd1(1) + crossProd2(2)), crossProd2(0) - crossProd1(1) + crossProd1(0) - crossProd2(0))
-        };
+    //     BLA::Matrix<4> q_0 = {
+    //         0.5 * sqrt(1 + a(0) + crossProd1(1) + crossProd2(2)),
+    //         0.5 * std::copysign(sqrt(1 + a(0) - crossProd1(1) - crossProd2(2)), crossProd2(1) - crossProd1(2)),
+    //         0.5 * std::copysign(sqrt(1 - a(0) + crossProd1(1) - crossProd2(2)), crossProd1(2) - crossProd2(1)),
+    //         0.5 * std::copysign(sqrt(1 - a(0) - crossProd1(1) + crossProd2(2)), crossProd2(0) - crossProd1(1) + crossProd1(0) - crossProd2(0))
+    //     };
 
-        Serial.println("<----- Initial Quaternion ----->");
-        for (int i = 0; i < q_0.Rows; i++) {
-            for (int j = 0; j < q_0.Cols; j++) {
-                Serial.print(String(q_0(i,j)) + "\t");
-            }
-            Serial.println("");
-        };
+    //     Serial.println("<----- Initial Quaternion ----->");
+    //     for (int i = 0; i < q_0.Rows; i++) {
+    //         for (int j = 0; j < q_0.Cols; j++) {
+    //             Serial.print(String(q_0(i,j)) + "\t");
+    //         }
+    //         Serial.println("");
+    //     };
 
-        BLA::Matrix<10> x_0 = {q_0(0), q_0(1), q_0(2), q_0(3), 0, 0, 0, 0, 0, 0};
-        this->stateEstimator->init(x_0, 0.025);
+    //     BLA::Matrix<10> x_0 = {q_0(0), q_0(1), q_0(2), q_0(3), 0, 0, 0, 0, 0, 0};
+    //     this->stateEstimator->init(x_0, 0.025);
         
-        Serial.println("[Prelaunch] Initialized EKF");
-    }
+    //     Serial.println("[Prelaunch] Initialized EKF");
+    // }
 }
 
 State *PreLaunch::nextState_impl()
